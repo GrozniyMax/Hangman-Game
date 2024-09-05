399f14002637bca088ff22f3f765b0af02a83d26
GrozniyMax <AzmEcmTzar@mail.ru>
2024-09-05 13:37:48 +0400

    refactor: Изменен интерфейс Clearable, для удобства работы
    Теперь классу, реализующему этот интерфейс достаточно очистить свои поля. Очистка полей этого класса, реализующий Clearable происходит автоматически.

--- src/main/java/backend/academy/Clearable.java
+++ src/main/java/backend/academy/Clearable.java
@@ -8,12 +8,12 @@ import java.util.Arrays;
 public interface Clearable {
 
     /**
-     * Очищает все модифицируемые поля, чтобы сделать перезапуск игры
+     * Очищает все модифицируемые поля, не реализующие интерфейс Clearable
      */
     void clear();
 
     /**
-     * Метод, который вызывает clear() во всех полях объекта, который наследует интерфейс Clearable
+     * Метод, который вызывает clear() во всех полях объекта, которые реализуют интерфейс Clearable
      */
     default void clearClearableFields(){
         Arrays.stream(this.getClass().getDeclaredFields())
@@ -22,11 +22,19 @@ public interface Clearable {
                     field.setAccessible(true);
                     Object fieldValue = field.get(this);
                     if (fieldValue instanceof Clearable) {
-                        ((Clearable) fieldValue).clear();
+                        ((Clearable) fieldValue).fullClear();
                     }
                 } catch (IllegalAccessException e) {
                     throw new RuntimeException(e);
                 }
             }));
+    }
+
+    /**
+     * Метод для полной очистки объекта
+     */
+    default void fullClear(){
+        clearClearableFields();
+        clear();
     }
 }
